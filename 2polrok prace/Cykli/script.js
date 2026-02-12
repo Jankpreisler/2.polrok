9626a10a0b1b9d9ac984b06e2045fd55e5b27c54
jan preisler <gamingacaumd@gmail.com>
2026-02-12 10:22:37 +0100

    asdads

---
+++ 2polrok prace/Cykli/script.js
@@ -0,0 +1,103 @@
+for(let i =1; i <=10; i++)
+{
+  console.log(i);
+}
+
+for(let i =5; i <=10; i++)
+{
+  console.log(i);
+}
+
+for(let i =10; i <=10; i--)
+{
+  console.log(i);
+}
+
+for(let i =0; i <=20; i++)
+{
+  console.log(i);
+}
+
+for(let i =1; i <=5; i++)
+{
+  console.log("JavaSCript");
+}
+
+
+// Uroven 2
+for (i = 0; i <= 20; i += 2) {
+
+
+  console.log(i);
+
+
+}
+
+for (o = 1; o <= 19; o += 2) {
+  console.log(o);
+}
+
+for (s = 0; s <= 20; s += 2) {
+  console.log(s);
+}
+
+for (d = 20; d <= 20; d -= 2) {
+  console.log(d);
+}
+
+for (d = 0; d <= 100; d += 10) {
+  console.log(d);
+}
+
+//Uroven 3
+
+for (i = 1; i <= 20; i++) {
+  if (i % 3 === 0) {
+      console.log(i);
+  }
+
+}
+
+for (i = 1; i <= 30; i++) {
+
+  if (i > 10) {
+      console.log(i);
+  }
+
+}
+
+for (i = 1; i <= 30; i++) {
+
+  if (i > 10) {
+      console.log(i);
+  }
+
+}
+
+for (i = 1; i <= 50; i++) {
+  if (i % 2 === 0) {
+      console.log(i);
+  }
+
+}
+
+for (i = 1; i <= 20; i++) {
+  if (i %5 !== 0) {
+      console.log(i);
+  }
+
+}
+
+for (i = 1; i <= 100; i++) {
+  if (i %4 === 0 && i <=50) {
+      console.log(i);
+  }
+
+}
+
+//Uroven 4
+for (i = 1; i <= 10; i++) 
+{
+  
+
+}
--- 2polrok prace/Zadaniezdna12.2/scripososijscisnt.js
+++ 2polrok prace/Zadaniezdna12.2/scripososijscisnt.js
@@ -109,5 +109,78 @@ switch (znamka) {
 }
 
 
+// Uroven 2
+
+for (i = 0; i <= 20; i += 2) {
+
+
+    console.log(i);
+
+
+}
+
+for (o = 1; o <= 19; o += 2) {
+    console.log(o);
+}
+
+for (s = 0; s <= 20; s += 2) {
+    console.log(s);
+}
+
+for (d = 20; d <= 20; d -= 2) {
+    console.log(d);
+}
+
+for (d = 0; d <= 100; d += 10) {
+    console.log(d);
+}
+
+
+for (i = 1; i <= 20; i++) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
+
+}
+
+for (i = 1; i <= 30; i++) {
+
+    if (i > 10) {
+        console.log(i);
+    }
+
+}
+
+for (i = 1; i <= 30; i++) {
+
+    if (i > 10) {
+        console.log(i);
+    }
+
+}
+
+for (i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+
+}
+
+for (i = 1; i <= 20; i++) {
+    if (i %5 !== 0) {
+        console.log(i);
+    }
+
+}
+
+for (i = 1; i <= 100; i++) {
+    if (i %4 === 0 && i <=50) {
+        console.log(i);
+    }
+
+}
+
+
+
 
 
