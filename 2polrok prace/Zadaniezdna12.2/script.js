5631dfe1bd970e11f44785a60e380d0061d6d77f
jan preisler <gamingacaumd@gmail.com>
2026-02-12 09:26:53 +0100

    Switch lesson

--- 2polrok prace/Zadaniezdna12.2/script.js
+++ 2polrok prace/Zadaniezdna12.2/script.js
@@ -1,53 +1,45 @@
 let age = 15;
 
-if (age >= 18) 
-{
+if (age >= 18) {
     console.log("Plnolety");
-} 
-else 
-{ 
-    console.log("Neplnolety"); 
+}
+else {
+    console.log("Neplnolety");
 }
 
 //-----------------------------
 
 let parita = 9.9;
 
-if (parita%2 ===0 && parita!==0) 
-{
+if (parita % 2 === 0 && parita !== 0) {
     console.log("Parne");
-} 
-else 
-{ 
-    console.log("Neparne"); 
+}
+else {
+    console.log("Neparne");
 }
 
 //------------------------------
 
 let score = 90;
 
-if (score >= 90) 
-{
+if (score >= 90) {
     console.log("Vyborny");
-} 
-else if (score >= 70)
-{ 
-    console.log("dobry"); 
 }
-else
-{
-    console.log("nedostatocny"); 
+else if (score >= 70) {
+    console.log("dobry");
+}
+else {
+    console.log("nedostatocny");
 }
 
 //--------------------------------
 
 let den = "Sobota";
 
-if (den === "Sobota" || den === "Nedela")
-{
+if (den === "Sobota" || den === "Nedela") {
     console.log("Je vikend");
 }
-else{
+else {
     console.log("Je tyzden");
 }
 
@@ -56,14 +48,13 @@ else{
 let vek = 15;
 let ticket = false;
 
-if (vek >= 15 && ticket === true)
-{
+if (vek >= 15 && ticket === true) {
     console.log("Moze vstupit");
 }
-else if (vek >=18 || ticket === false){
+else if (vek >= 18 || ticket === false) {
     console.log("Moze vstupit");
 }
-else{
+else {
     console.log("Nesmie ist dnu");
 }
 
@@ -71,13 +62,11 @@ else{
 
 let blocked = true;
 
-if (blocked !== true)
-{
-console.log("NIESi blokovani")
+if (blocked !== true) {
+    console.log("NIESi blokovani")
 
 }
-else
-{
+else {
     console.log("Si blokovani");
 }
 
@@ -85,15 +74,40 @@ else
 
 let number = 12;
 
-if (number%3 === 0)
-{
+if (number % 3 === 0) {
 
     console.log("Je delitelne")
 }
-else
-{
+else {
     console.log(number)
 }
+
+//-------------------------------------
+// Switch
+
+let znamka = 2;
+
+switch (znamka) {
+    case 1: console.log("Vyborny")
+        break;
+
+    case 2: console.log("Chvalitebny")
+        break;
+
+    case 3: console.log("Dobry")
+        break;
+
+    case 4: console.log("Dostatocny")
+        break;
+
+    case 5: console.log("Nedostatocny")
+        break;
+    default:
+        console.log("Zla znamka")
+        break;
+
+}
+
 
 
 
